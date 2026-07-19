e5180ebd7dcf7f2f86407e6e68494cdcf71038fe
continue <3327842846@qq.com>
2026-07-19 15:42:02 +0800

    构建AiService接口，连接通义千问进行测试

---
+++ src/main/java/com/Liang/java/ai/langchain4j/assistant/Assistant.java
@@ -0,0 +1,5 @@
+package com.Liang.java.ai.langchain4j.assistant;
+
+public interface Assistant {
+    String chat(String userMessage);
+}
---
+++ src/test/java/com/Liang/java/ai/langchain4j/AIServiceTest.java
@@ -0,0 +1,25 @@
+package com.Liang.java.ai.langchain4j;
+
+
+import com.Liang.java.ai.langchain4j.assistant.Assistant;
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.spring.AiService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AIServiceTest {
+
+    @Autowired
+
+    private QwenChatModel qwenChatModel;
+
+    @Test
+    public void testChat(){
+        Assistant assistant = AiServices.create(Assistant.class, qwenChatModel);
+        String response = assistant.chat("你好");
+        System.out.println(response);
+    }
+}
