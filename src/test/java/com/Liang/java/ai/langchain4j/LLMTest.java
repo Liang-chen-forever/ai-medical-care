c0b5f901e1154fa307c5d157f044b733380e3a65
continue <3327842846@qq.com>
2026-07-19 15:27:59 +0800

    接入ollama模型和阿里百炼平台的通义千问qwen-max模型和Wanx文生图模型

--- src/test/java/com/Liang/java/ai/langchain4j/LLMTest.java
+++ src/test/java/com/Liang/java/ai/langchain4j/LLMTest.java
@@ -1,10 +1,19 @@
 package com.Liang.java.ai.langchain4j;
 
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.community.model.dashscope.WanxImageModel;
+import dev.langchain4j.data.image.Image;
+import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.output.Response;
+import net.bytebuddy.agent.VirtualMachine;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.awt.*;
+import java.net.URI;
 
 @SpringBootTest
 public class LLMTest {
@@ -23,7 +32,6 @@ public class LLMTest {
         System.out.println(response);
     }
 
-
     @Autowired
     private OpenAiChatModel model;
 
@@ -31,5 +39,38 @@ public class LLMTest {
     public void testSpringBoot(){
         String response = model.chat("你好");
         System.out.println(response);
+    }
+
+    @Autowired
+    private OllamaChatModel ollamaModel;
+
+    @Test
+    public void testOllama(){
+        String response = ollamaModel.chat("你好");
+        System.out.println(response);
+    }
+
+    @Autowired
+    private QwenChatModel qwenModel;
+
+    @Test
+    public void testQwen(){
+        String response = qwenModel.chat("你好");
+        System.out.println(response);
+    }
+
+
+    // 测试DashScope Wanx模型---文生图模型
+    @Test
+    public void testDashScopeWanx(){
+        WanxImageModel wanxImageModel = WanxImageModel
+                .builder()
+                .modelName("wanx2.1-t2i-turbo")
+                .apiKey("${DASH_SCOPE_API_KEY}")
+                .build();
+
+        Response<Image> response = wanxImageModel.generate("");
+        URI url = response.content().url();
+        System.out.println(url);
     }
 }
