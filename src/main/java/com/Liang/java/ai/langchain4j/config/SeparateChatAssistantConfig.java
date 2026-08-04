e67b665f4e933725427431c9f91856b77c667c07
continue <3327842846@qq.com>
2026-08-04 14:14:03 +0800

    实现替换聊天记忆的内容，缺点都是不能磁盘持久化，关机后对话记忆消失

--- src/main/java/com/Liang/java/ai/langchain4j/config/SeparateChatAssistantConfig.java
+++ src/main/java/com/Liang/java/ai/langchain4j/config/SeparateChatAssistantConfig.java
@@ -4,6 +4,7 @@ package com.Liang.java.ai.langchain4j.config;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,11 @@ public class SeparateChatAssistantConfig {
 
     public ChatMemoryProvider chatMemoryProvider(){
         //实现自动注入memoryId
-        return memoryId -> MessageWindowChatMemory.builder().id(memoryId).maxMessages(10).build();
+        return memoryId -> MessageWindowChatMemory
+                .builder()
+                .id(memoryId)
+                .maxMessages(10)
+                //.chatMemoryStore(new InMemoryChatMemoryStore()) //都是存储在内容当中，不能持久化记忆
+                .build();
     }
 }
